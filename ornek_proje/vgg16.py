91c8f43d1b45cfb78d7a2c24e957f96b88db93a7
Mehmetcan Karabulut <48991628+MehmetcanKarabulut@users.noreply.github.com>
2020-01-03 17:50:56 +0200

    Create vgg16.py

---
+++ ornek_proje/vgg16.py
@@ -0,0 +1,489 @@
+%tensorflow_version 1.x
+import numpy as np 
+from tqdm import tqdm
+import cv2
+import os
+import shutil
+import itertools
+import imutils
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+import plotly.graph_objs as go
+from plotly.offline import init_notebook_mode, iplot
+from plotly import tools
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras import layers
+from keras.models import Model, Sequential
+from keras.optimizers import Adam, RMSprop
+from keras.callbacks import EarlyStopping
+
+def load_data(dir_path, img_size=(100,100)):
+    """
+    Load resized images as np.arrays to workspace
+    """
+    X = []
+    y = []
+    i = 0
+    labels = dict()
+    for path in tqdm(sorted(os.listdir(dir_path))):
+        if not path.startswith('.'):
+            labels[i] = path
+            for file in os.listdir(dir_path + path):
+                if not file.startswith('.'):
+                    img = cv2.imread(dir_path + path + '/' + file)
+                    X.append(img)
+                    y.append(i)
+            i += 1
+    X = np.array(X)
+    y = np.array(y)
+    print(f'{len(X)} images loaded from {dir_path} directory.')
+    return X, y, labels
+
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.figure(figsize = (6,6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    cm = np.round(cm,2)
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+def plot_samples(X, y, labels_dict, n=50):
+    """
+    Creates a gridplot for desired number of images (n) from the specified set
+    """
+    for index in range(len(labels_dict)):
+        imgs = X[np.argwhere(y == index)][:n]
+        j = 10
+        i = int(n/j)
+
+        plt.figure(figsize=(15,6))
+        c = 1
+        for img in imgs:
+            plt.subplot(i,j,c)
+            plt.imshow(img[0])
+
+            plt.xticks([])
+            plt.yticks([])
+            c += 1
+        plt.suptitle('Tumor: {}'.format(labels_dict[index]))
+        plt.show()
+
+def crop_imgs(set_name, add_pixels_value=0):
+    """
+    Finds the extreme points on the image and crops the rectangular out of them
+    """
+    set_new = []
+    for img in set_name:
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        # threshold the image, then perform a series of erosions +
+        # dilations to remove any small regions of noise
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        # find contours in thresholded image, then grab the largest one
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        c = max(cnts, key=cv2.contourArea)
+
+        # find the extreme points
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+        ADD_PIXELS = add_pixels_value
+        new_img = img[extTop[1]-ADD_PIXELS:extBot[1]+ADD_PIXELS, extLeft[0]-ADD_PIXELS:extRight[0]+ADD_PIXELS].copy()
+        set_new.append(new_img)
+
+    return np.array(set_new)
+
+def save_new_images(x_set, y_set, folder_name):
+    i = 0
+    for (img, imclass) in zip(x_set, y_set):
+        if imclass == 0:
+            cv2.imwrite(folder_name+'NO/'+str(i)+'.jpg', img)
+        else:
+            cv2.imwrite(folder_name+'YES/'+str(i)+'.jpg', img)
+        i += 1
+
+def preprocess_imgs(set_name, img_size):
+    """
+    Resize and apply VGG-15 preprocessing
+    """
+    set_new = []
+    for img in set_name:
+        img = cv2.resize(
+            img,
+            dsize=img_size,
+            interpolation=cv2.INTER_CUBIC
+        )
+        set_new.append(preprocess_input(img))
+    return np.array(set_new)
+
+init_notebook_mode(connected=True)
+RANDOM_SEED = 123
+
+#                                                                   data import
+
+
+
+"""
+IMG_PATH = '/content/gdrive/My Drive/proje/dataset/'
+# split the data by train/val/test
+for CLASS in os.listdir(IMG_PATH):
+    if not CLASS.startswith('q'):
+        IMG_NUM = len(os.listdir(IMG_PATH + CLASS))
+        for (n, FILE_NAME) in enumerate(os.listdir(IMG_PATH + CLASS)):
+            img = IMG_PATH + CLASS + '/' + FILE_NAME
+            if n < 5:
+                shutil.copy(img, '/content/gdrive/My Drive/proje/dataset/qTEST/' + CLASS.upper() + '/' + FILE_NAME)
+            elif n < 0.8*IMG_NUM:
+                shutil.copy(img, '/content/gdrive/My Drive/proje/dataset/qTRAIN/'+ CLASS.upper() + '/' + FILE_NAME)
+            else:
+                shutil.copy(img, '/content/gdrive/My Drive/proje/dataset/qVAL/'+ CLASS.upper() + '/' + FILE_NAME)
+"""
+
+
+TRAIN_DIR = '/content/gdrive/My Drive/proje/dataset/qTRAIN/'
+TEST_DIR = '/content/gdrive/My Drive/proje/dataset/qTEST/'
+VAL_DIR = '/content/gdrive/My Drive/proje/dataset/qVAL/'
+IMG_SIZE = (224,224)
+
+# use predefined function to load the image data into workspace
+X_train, y_train, labels = load_data(TRAIN_DIR, IMG_SIZE)
+X_test, y_test, _ = load_data(TEST_DIR, IMG_SIZE)
+X_val, y_val, _ = load_data(VAL_DIR, IMG_SIZE)
+
+
+y = dict()
+y[0] = []
+y[1] = []
+for set_name in (y_train, y_val, y_test):
+    y[0].append(np.sum(set_name == 0))
+    y[1].append(np.sum(set_name == 1))
+
+trace0 = go.Bar(
+    x=['Train Set', 'Validation Set', 'Test Set'],
+    y=y[0],
+    name='No',
+    marker=dict(color='#33cc33'),
+    opacity=0.7
+)
+trace1 = go.Bar(
+    x=['Train Set', 'Validation Set', 'Test Set'],
+    y=y[1],
+    name='Yes',
+    marker=dict(color='#ff3300'),
+    opacity=0.7
+)
+data = [trace0, trace1]
+layout = go.Layout(
+    title='Count of classes in each set',
+    xaxis={'title': 'Set'},
+    yaxis={'title': 'Count'}
+)
+fig = go.Figure(data, layout)
+iplot(fig)
+print("original images")
+plot_samples(X_train, y_train, labels, 10)
+
+
+"""
+RATIO_LIST = []
+for set in (X_train, X_test, X_val):
+    for img in set:
+        RATIO_LIST.append(img.shape[1]/img.shape[0])
+        
+plt.hist(RATIO_LIST)
+plt.title('Distribution of Image Ratios')
+plt.xlabel('Ratio Value')
+plt.ylabel('Count')
+plt.show()
+
+"""
+
+#                                                             data normalization
+
+"""
+# crop one image
+img = cv2.imread('/content/gdrive/My Drive/proje/dataset/yes/Y108.jpg')
+img = cv2.resize(
+            img,
+            dsize=IMG_SIZE,
+            interpolation=cv2.INTER_CUBIC
+        )
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+# threshold the image, then perform a series of erosions +
+# dilations to remove any small regions of noise
+thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.erode(thresh, None, iterations=2)
+thresh = cv2.dilate(thresh, None, iterations=2)
+
+# find contours in thresholded image, then grab the largest one
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+c = max(cnts, key=cv2.contourArea)
+
+# find the extreme points
+extLeft = tuple(c[c[:, :, 0].argmin()][0])
+extRight = tuple(c[c[:, :, 0].argmax()][0])
+extTop = tuple(c[c[:, :, 1].argmin()][0])
+extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+# add contour on the image
+img_cnt = cv2.drawContours(img.copy(), [c], -1, (0, 255, 255), 4)
+
+# add extreme points
+img_pnt = cv2.circle(img_cnt.copy(), extLeft, 8, (0, 0, 255), -1)
+img_pnt = cv2.circle(img_pnt, extRight, 8, (0, 255, 0), -1)
+img_pnt = cv2.circle(img_pnt, extTop, 8, (255, 0, 0), -1)
+img_pnt = cv2.circle(img_pnt, extBot, 8, (255, 255, 0), -1)
+
+# crop
+ADD_PIXELS = 0
+new_img = img[extTop[1]-ADD_PIXELS:extBot[1]+ADD_PIXELS, extLeft[0]-ADD_PIXELS:extRight[0]+ADD_PIXELS].copy()
+
+plt.figure(figsize=(15,6))
+plt.subplot(141)
+plt.imshow(img)
+plt.xticks([])
+plt.yticks([])
+plt.title('Step 1. Get the original image')
+plt.subplot(142)
+plt.imshow(img_cnt)
+plt.xticks([])
+plt.yticks([])
+plt.title('Step 2. Find the biggest contour')
+plt.subplot(143)
+plt.imshow(img_pnt)
+plt.xticks([])
+plt.yticks([])
+plt.title('Step 3. Find the extreme points')
+plt.subplot(144)
+plt.imshow(new_img)
+plt.xticks([])
+plt.yticks([])
+plt.title('Step 4. Crop the image')
+plt.show()
+
+"""
+
+
+# crop each set
+X_train_crop = crop_imgs(set_name=X_train)
+X_val_crop = crop_imgs(set_name=X_val)
+X_test_crop = crop_imgs(set_name=X_test)
+
+print("cropped images")
+plot_samples(X_train_crop, y_train, labels, 10)
+
+
+"""
+# saving new images to the folder
+
+save_new_images(X_train_crop, y_train, folder_name='/content/gdrive/My Drive/proje/dataset/qTRAIN_CROP/')
+save_new_images(X_val_crop, y_val, folder_name='/content/gdrive/My Drive/proje/dataset/qVAL_CROP/')
+save_new_images(X_test_crop, y_test, folder_name='/content/gdrive/My Drive/proje/dataset/qTEST_CROP/')
+
+"""
+
+# resizing images to (224,224) and applying preprocessing needed for VGG-16 model input
+
+X_train_prep = preprocess_imgs(set_name=X_train_crop, img_size=IMG_SIZE)
+X_test_prep = preprocess_imgs(set_name=X_test_crop, img_size=IMG_SIZE)
+X_val_prep = preprocess_imgs(set_name=X_val_crop, img_size=IMG_SIZE)
+
+print("converted 224x224 RGB image")
+plot_samples(X_train_prep, y_train, labels, 10)
+
+# veri artırma
+TRAIN_DIR = '/content/gdrive/My Drive/proje/dataset/qTRAIN_CROP/'
+VAL_DIR = '/content/gdrive/My Drive/proje/dataset/qVAL_CROP/'
+
+train_datagen = ImageDataGenerator(
+    rotation_range=15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.1,
+    brightness_range=[0.5, 1.5],
+    horizontal_flip=True,
+    vertical_flip=True,
+    preprocessing_function=preprocess_input
+)
+
+test_datagen = ImageDataGenerator(
+    preprocessing_function=preprocess_input
+)
+
+
+train_generator = train_datagen.flow_from_directory(
+    TRAIN_DIR,
+    color_mode='rgb',
+    target_size=IMG_SIZE,
+    batch_size=32,
+    class_mode='binary',
+    seed=RANDOM_SEED
+)
+
+
+validation_generator = test_datagen.flow_from_directory(
+    VAL_DIR,
+    color_mode='rgb',
+    target_size=IMG_SIZE,
+    batch_size=16,
+    class_mode='binary',
+    seed=RANDOM_SEED
+)
+
+
+#                                                                   CNN Model
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.models import Model
+import matplotlib.pyplot as plt
+from numpy import expand_dims
+from matplotlib import pyplot 
+
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Dropout, Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+import keras
+
+# vgg16_weight_path = 'content/gdrive/My Drive/proje/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+vgg = VGG16(
+    weights='imagenet',
+    include_top=False, 
+    input_shape=IMG_SIZE + (3,)
+)
+
+NUM_CLASSES = 1
+
+vgg16 = Sequential()
+vgg16.add(vgg)
+vgg16.add(layers.Dropout(0.3))
+vgg16.add(layers.Flatten())
+vgg16.add(layers.Dropout(0.5))
+vgg16.add(layers.Dense(NUM_CLASSES, activation='sigmoid'))
+
+vgg16.layers[0].trainable = False
+
+vgg16.compile(
+    loss='binary_crossentropy',
+    optimizer=RMSprop(lr=1e-4),
+    metrics=['accuracy']
+)
+vgg16.compile(loss='binary_crossentropy', optimizer=keras.optimizers.Adam(lr=0.0003, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False), metrics=["accuracy"])
+
+# vgg16.summary()
+
+"""
+# load the model
+model = VGG16()
+# redefine model to output right after the first hidden layer
+ixs = [2, 5, 9, 13, 17]
+outputs = [model.layers[i].output for i in ixs]
+model = Model(inputs=model.inputs, outputs=outputs)
+# load the image with the required shape
+# convert the image to an array
+img = img_to_array(X_val_prep[43])
+# expand dimensions so that it represents a single 'sample'
+img = expand_dims(img, axis=0)
+# prepare the image (e.g. scale pixel values for the vgg)
+img = preprocess_input(img)
+# get feature map for first hidden layer
+feature_maps = model.predict(img)
+# plot the output from each block
+square = 8
+for fmap in feature_maps:
+	# plot all 64 maps in an 8x8 squares
+	ix = 1
+	for _ in range(square):
+		plt.figure(figsize=(64,64))
+		for _ in range(square):
+           
+
+			# specify subplot and turn of axis
+			ax = pyplot.subplot(square, square, ix)
+			ax.set_xticks([])
+			ax.set_yticks([])
+			
+			# plot filter channel in grayscale
+			plt.imshow(fmap[0, :, :, ix-1], cmap='viridis')
+			ix += 1
+	# show the figure
+
+        
+	plt.show()
+"""
+ 
+vgg16_history = vgg16.fit_generator(
+    train_generator,
+    steps_per_epoch=50,
+    epochs=80,
+    validation_data=validation_generator,
+    validation_steps=30,
+)
+
+# validate on val set
+predictions = vgg16.predict(X_test_prep)
+predictions = [1 if x>0.5 else 0 for x in predictions]
+
+_, train_acc = vgg16.evaluate(X_val_prep, y_val, verbose=0)
+_, test_acc = vgg16.evaluate(X_test_prep, y_test, verbose=0)
+
+pyplot.figure(figsize=(12,12))
+# plot loss during training
+pyplot.subplot(211)
+pyplot.title('Vgg16 Loss')
+pyplot.plot(vgg16_history.history['loss'], label='train')
+pyplot.plot(vgg16_history.history['val_loss'], label='Validation')
+pyplot.legend()
+# plot accuracy during training
+pyplot.subplot(212)
+pyplot.title('Vgg16 Accuracy')
+pyplot.plot(vgg16_history.history['acc'], label='train')
+pyplot.plot(vgg16_history.history['val_acc'], label='Validation')
+pyplot.legend()
+pyplot.show()
+
+print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
